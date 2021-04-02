b921e995fd0dfeb64c0953eb8ac3ea33f467cf7a
sanhyu <qimsh7@gmail.com>
2021-04-03 04:30:03 +0900

    Add (delete) & (complete) to [main.html] & [app.js]

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
 var todo = document.getElementById('todo')
+var addBtn = document.getElementById('addBtn')
+var deleteBtn = document.getElementById('deleteBtn')
 var inputText = document.getElementById('inputText');
 
-function addContent(){
+var createListItemElements = function(text){
   var content = document.createElement('li');
-  content.innerText = inputText.value;
-  todo.appendChild(content);
+  content.innerHTML = text;
+  content.id = 'content';
+
+  var editBtn=  document.createElement('button');
+  editBtn.innerHTML = 'Edit';
+
+  var deleteBtn = document.createElement('button');
+  deleteBtn.innerHTML = 'Delete';
+  deleteBtn.id = 'deleteBtn';
+  deleteBtn.addEventListener('click', delListItem);
+
+  content.appendChild(editBtn);
+  content.appendChild(deleteBtn);
+
+  return content;
+}
+
+var addListItem = function(){
+  var listItem = createListItemElements(inputText.value);
+  todo.appendChild(listItem);
+  inputText.value='';
+}
+addBtn.addEventListener('click', addListItem);
+
+var delListItem = function(){
+  var listItem = document.getElementById('content');
+  listItem.parentNode.removeChild(listItem);
 }
